9f61434e036a9c238e32759eb8ba334ea81122c1
HiddenBoiRightHere <aden8419c@gmail.com>
2025-04-04 14:57:27 +0200

    getting more data and checking other info

--- data_extractor.py
+++ data_extractor.py
@@ -1,4 +1,6 @@
 import ast
+import datetime
+import json
 import pandas as pd
 import numpy as np
 from pathlib import Path
@@ -8,11 +10,7 @@ import matplotlib.dates as mdates
 from typing import Optional
 import os
 
-
-## Collect heart rate zones, sleep data, and physical activity (determine if can use hr zones)
-
-## make a function to make this easier
-
+########## LOAD DATA ##########
 def depack_df(path: Path, key: Optional[str] = None, tag: Optional[str] = None):
     """
     Collects information from what is intended to be a csv file and expands out most values into new columns for use elsewhere.
@@ -37,7 +35,29 @@ def depack_df(path: Path, key: Optional[str] = None, tag: Optional[str] = None):
         df_set = df_temp
 
     # set value column to dictionary
-    df_set.loc[:, "Value"] = df_set["Value"].apply(ast.literal_eval)
+    try:
+        df_set.loc[:, "Value"] = df_set["Value"].apply(ast.literal_eval)
+    except Exception as e:
+        # WHAT DO YOU MEAN MALFORMED STRING
+        # IT'S ALL AUTOMATIC
+        # THERE IS NO MALFORMED STRING IN BA SING SE
+        print(path)
+        print(e)
+
+        # Does cool prints but guess waht that takes time so deal with it later
+        # for idx, value in enumerate(df_set['Value']):
+        #     try:
+        #         ast.literal_eval(value)
+        #     except (ValueError, SyntaxError) as e:
+        #         print(f"Error at row {idx}: {repr(value)} - {e}")
+        #         print(df_set['Value'].apply(type).value_counts())
+        #         for i, char in enumerate(value):
+        #             try:
+        #                 ast.literal_eval(value[:i + 1])  # Check incrementally
+        #             except Exception as sub_e:
+        #                 print(f"Fails at position {i}: {repr(char)} - {sub_e}")
+        #                 break  # Stop at the first broken character
+
 
     # Convert dictionaries to new df
     df_outer = pd.json_normalize(df_set["Value"])
@@ -59,18 +79,66 @@ def depack_df(path: Path, key: Optional[str] = None, tag: Optional[str] = None):
     return(df_outer)
 
 
-# set paths for the csvs to be loaded
-# TODO: automate loading paths and associated dataframe filtering settings
-# note that agg and fit get used twice to get sleep + hr data...
 
-# open folder
-# for items in folder, if item ends in """""", do process
+## Create empty dataframes to concat to
+df_heartrate = pd.DataFrame()
+df_sleep = pd.DataFrame()
+df_sport_info = pd.DataFrame()
+df_hr_fine = pd.DataFrame()
+df_sleep_fine = pd.DataFrame()
+
+# Get filenames in data directory
 for filename in os.listdir(os.getcwd()+"\data"):
-   print(filename)
 
-hlthc_agg = Path("hlth_center_aggregated_fitness_data.csv")
-hlthc_sport = Path("hlth_center_sport_record.csv")
-hlthc_fit = Path("hlth_center_fitness_data.csv")
+    if "hlth_center_aggregated_fitness_data.csv" in filename:
+        hlthc_agg = Path("data\\"+filename)
+
+        df_heartrate = pd.concat([df_heartrate, depack_df(hlthc_agg, key="heart_rate", tag="daily_report")], ignore_index=True)
+        df_sleep = pd.concat([df_sleep, depack_df(hlthc_agg, key="sleep", tag="daily_report")], ignore_index=True)
+
+    elif "hlth_center_sport_record.csv" in filename:
+        hlthc_sport = Path("data\\" + filename)
+        df_sport_info = pd.concat([df_sport_info, depack_df(hlthc_sport)], ignore_index=True)
+
+    elif "hlth_center_fitness_data.csv" in filename:
+        hlthc_fit = Path("data\\" + filename)
+        df_hr_fine = pd.concat([df_hr_fine, depack_df(hlthc_fit, key="heart_rate")], ignore_index=True)
+        df_sleep_fine = pd.concat([df_sleep_fine, depack_df(hlthc_fit, key="sleep")], ignore_index=True)
+
+    elif "Weather" in filename:
+        df_weather = pd.read_csv(Path("data\\" + filename))
+
+for filename in os.listdir(os.getcwd()+"\data\sgar"):
+
+    if "heart_rate" in filename:
+        with open(Path("data\\sgar\\" + filename)) as f:
+            data = json.load(f)
+
+        # Extract only heartRateValues
+        heart_rate_values = data.get("heartRateValues", [])
+
+        # Convert to DataFrame
+        df_hr_fine_sgar = pd.DataFrame(heart_rate_values, columns=["timestamp", "heartRate"])
+
+        # Convert timestamp from milliseconds to seconds
+        df_hr_fine_sgar["timestamp"] = df_hr_fine_sgar["timestamp"] // 1000  # Integer division
+
+        # Convert timestamp to datetime
+        df_hr_fine_sgar["timestamp"] = pd.to_datetime(df_hr_fine_sgar["timestamp"], unit="s")
+
+        # Display first few rows
+        print(df_hr_fine_sgar.head())
+
+    elif "Sleep" in filename:
+        print("sleep")
+
+    elif "walking" in filename:
+        df_sport_info_sgar = pd.read_csv(Path("data\\sgar\\" + filename))
+        print(df_sport_info_sgar.head())
+
+
+
+########## PROCESS ##########
 
 #### RELEVANT DFs:
 # df_heartrate: contains aggregated hr data from daily report
@@ -79,31 +147,41 @@ hlthc_fit = Path("hlth_center_fitness_data.csv")
 # df_hr_fine: contains raw measurements of hr throughout the day
 # df_sleep_fine: contains raw measurements of sleep
 
-# set dataframes
-df_heartrate = depack_df(hlthc_agg, key="heart_rate", tag="daily_report")
-df_sleep = depack_df(hlthc_agg, key="sleep", tag="daily_report")
-df_sport_info = depack_df(hlthc_sport)
-df_hr_fine = depack_df(hlthc_fit, key="heart_rate")
-df_sleep_fine = depack_df(hlthc_fit, key="sleep")
-df_weather = pd.read_csv(Path("data/Daily Weather Data.csv"))
+# Collect overlap times in data
+unique_ids = df_heartrate["Uid"].unique()
+min_times = []
+max_times = []
 
-print(df_weather.head)
+# Collect min and max times for each student
+for ids in unique_ids:
+    min_times.append(min(df_hr_fine[df_hr_fine["Uid"] == ids]["Time"]))
+    max_times.append(max(df_hr_fine[df_hr_fine["Uid"] == ids]["Time"]))
 
-# print(df_heartrate.iloc[0])
-# print(df_sleep.iloc[0])
-# print(df_sport_info.iloc[1])
-# print(df_hr_fine.iloc[0])
-# print(df_sleep_fine.iloc[0])
+# Print min max days
+print("Overlap", max(min_times), min(max_times))
+
+# Collect time difference
+setup = [max(min_times), min(max_times)]
+datetime_objects = pd.to_datetime(setup)
+time_difference = max(datetime_objects) - min(datetime_objects)
+print(f"Time difference: {time_difference}")
+
+
+print(len(df_sport_info))
+
+print(df_sleep.iloc[0])
+
 
 
 
 ## If i have time...
-# TODO: make visualizations of different attributes for the data in regards to RQ
-# TODO: determine if possible to extract heart rate zone per hour vs. just through the day? (ask tutor which is easier)
+# TODO: it is possible to extract heart rate zone per hour vs. just through the day?
+# TODO: note how the app calculates hr zones and include it in report
 
-## MAIN PRIORITIES
-# TODO: get data start and end points for datetime (so we have our 2wk+ measurements overlap) determine mathematically via datetime
-# TODO: Check sam watch data (get single day data)
+## PRIORITY
+# TODO: begin making linregs
+# TODO: get+merge sam dataset
+# TODO: visualizations
 
 
 # Zoltan
